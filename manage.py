86a4260ee3f28662323629e0d5e47ef496a9fe5b
Alva <alvamaker@163.com>
2022-08-12 18:04:05 +0800

    添加配置文件夹 区分开发环境

--- manage.py
+++ manage.py
@@ -6,15 +6,19 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meiduo_mall.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meiduo_mall.settings.develop')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from exc
+        raise
     execute_from_command_line(sys.argv)
 
 
